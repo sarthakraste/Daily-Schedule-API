7b56cd9e20d896d34ac4f6f48a46e9d0589bd8d0
S D R <sarthakraste@yahoo.in>
2016-01-05 17:52:22 -0600

    Post todos

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 var express = require('express');
+var bodyparser=require('body-parser');
+
 var app = express();
 
 var PORT = process.env.PORT || 3000;
-var todos = [{
-id: 1,
-description: 'Meet Mom for Lunch',
-completed: false
-},{
-id: 2,
-description:'Go to Market',
-completed: false
-},{
-id:3,
-description:'Go Home',
-completed:true
-}];
+var todos = [];
+var todonextid = 1;
 
-
+app.use(bodyparser.json());
 app.get('/', function (req,res) {
 
 res.send('Todo API Root');
@@ -48,6 +39,19 @@ res.json(matchedtodo);
 //res.send('Asking for todo with id of ' + req.params.id);
 
 });
+
+
+ app.post('/todos', function (req, res){
+
+var body = req.body;
+body.id=todonextid;
+todonextid++;
+
+todos.push(body);
+//console.log('description: ' +  body.description);
+ 
+res.json(body);
+ });
 
 app.listen(PORT, function () {
 
