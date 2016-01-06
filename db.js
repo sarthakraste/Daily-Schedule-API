2f83d869e98374551ab3588829edb4aeae03982b
S D R <sarthakraste@yahoo.in>
2016-01-06 00:15:01 -0600

    db update 4

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
 var sequelize = require('sequelize');
 
-var seq = new sequelize(undefined, undefined, undefined, {
+var env= process.env,NODE_ENV || 'development';
+var seq
+if(env==='production') {
+
+seq = new sequelize(process.env.DATABASE_URL, {
+
+	dialect:'postgres'
+});
+
+} else {
+ seq = new sequelize(undefined, undefined, undefined, {
 
 	'dialect':'sqlite',
 	'storage': __dirname +'/data/dev=todo-api.sqlite'
 });
-
+}
 var db = {};
 db.todo = seq.import(__dirname + '/models/todo.js')
 db.seq= seq;
