29d101fb1d04ff6070573479551f2adc28c73dcd
S D R <sarthakraste@yahoo.in>
2016-01-05 19:08:44 -0600

    refactored

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyparser=require('body-parser');
+var _=require('underscore');
 
 var app = express();
 
@@ -23,13 +24,8 @@ res.json(todos)
 
 app.get('/todos/:id', function (req,res){
 	var todoid = parseInt(req.params.id,10);
-	var matchedtodo;
-todos.forEach(function (todo){
-if(todoid===todo.id){
+	var matchedtodo=_.findWhere(todos, {id:todoid});
 
-matchedtodo = todo;
-}
-}) ;
 if (matchedtodo){
 res.json(matchedtodo);
 } else {
@@ -42,8 +38,13 @@ res.json(matchedtodo);
 
 
  app.post('/todos', function (req, res){
-
 var body = req.body;
+body=_.pick(body, 'description', 'completed')
+if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+
+	return res.status(404).send();
+}
+body.description=body.description.trim();
 body.id=todonextid;
 todonextid++;
 
